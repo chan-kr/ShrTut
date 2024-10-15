d4aaf118e96fbd97c6ffe8ab402ee72d07ea43bc
chanKR <chanasdeveloper@gmail.com>
2024-10-15 22:56:00 +0900

    2nd Commit - 조건문 공부 (1) - if문

--- Program.cs
+++ Program.cs
@@ -7,7 +7,29 @@ namespace ShrTut
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //2일차 조건문 공부(1) - if문 
+            Console.WriteLine("숫자를 입력하세요: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine("입력한 숫자는 양수입니다.");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("입력한 숫자는 음수입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("입력한 숫자는 0입니다.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("올바른 숫자를 입력해주세요.");
+            }
         }
     }
 }
