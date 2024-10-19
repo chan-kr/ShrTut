61d16d9418f1bb5d1650f7b025c47cecab141c78
chanKR <chanasdeveloper@gmail.com>
2024-10-19 19:46:28 +0900

    6th Commit - 반복문 공부(1) - for 문

--- Program.cs
+++ Program.cs
@@ -7,38 +7,14 @@ namespace ShrTut
     {
         static void Main(string[] args)
         {
-            // C# 기본문법 - 제어문(2) - switch문
-            Console.WriteLine("1부터 5 사이의 숫자를 입력하세요: ");
-            string input = Console.ReadLine();
+            Console.WriteLine("1부터 10까지의 숫자를 출력합니다:");
 
-            if (int.TryParse(input, out int number))
+            for (int i = 1; i <= 10; i++)
             {
-                switch (number)
-                {
-                    case 1:
-                        Console.WriteLine("매우 나쁨");
-                        break;
-                    case 2:
-                        Console.WriteLine("나쁨");
-                        break;
-                    case 3:
-                        Console.WriteLine("보통");
-                        break;
-                    case 4:
-                        Console.WriteLine("좋음");
-                        break;
-                    case 5:
-                        Console.WriteLine("매우 좋음");
-                        break;
-                    default:
-                        Console.WriteLine("1부터 5 사이의 숫자를 입력해주세요.");
-                        break;
-                }
+                Console.WriteLine(i);
             }
-            else
-            {
-                Console.WriteLine("올바른 숫자를 입력해주세요.");
-            }
+
+            Console.WriteLine("for문이 종료되었습니다.");
         }
     }
 }
