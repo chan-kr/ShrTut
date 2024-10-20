9d090ac7949097e3d1172e91b2e3d3c4ec5b2e09
chanKR <chanasdeveloper@gmail.com>
2024-10-20 20:41:13 +0900

    8th Commit - Class 공부(1) - Generic

--- Program.cs
+++ Program.cs
@@ -3,17 +3,24 @@ using System;
 
 namespace ShrTut
 {
+
+    public class Item<T>
+    {
+        // Generic type parameter
+        public T content { get; set; }
+
+        public Item(T content)
+        {
+            this.content = content;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 1;
-            
-            while (i <= 10)
-            {
-                Console.WriteLine(i);
-                i++;
-            }
+
         }
     }
+
+
 }
